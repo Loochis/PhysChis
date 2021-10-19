0b9f1cd68c395cd5db73446623f782a0082cede4
Loochsi <lucas.disarra@gmail.com>
2021-10-19 10:06:56 -0400

    Some Comments

--- PhysChisObjects.py
+++ PhysChisObjects.py
@@ -33,12 +33,15 @@ class Object:
         self.objCollection = objects
         self.fieldCollection = fields
 
+    # Returns the velocity of the object
     def GetVelocity(self) -> Vector3:
         return self.velocity
     
+    # Returns the momentum of the object
     def GetMomentum(self) -> Vector3:
         return self.GetVelocity() * self.mass
 
+    # Returns the net acceleration due to fields on the generic object, TO BE OVERRIDEN IN SUBCLASSES
     def GetNetAcceleration(self) -> Vector3:
         netAcc = Vector3.Zero()
         for field in self.fieldCollection:
@@ -46,12 +49,15 @@ class Object:
                 netAcc += field.acceleration
         return netAcc
 
+    # Returns the net force acting on the object
     def GetNetForce(self) -> Vector3:
         return self.GetNetAcceleration() * self.mass
 
+    # Returns the net impulse acting on the object
     def GetNetImpulse(self) -> Vector3:
         return self.GetNetForce() * C.TIMESTEP
 
+    # Updates the position and velocity of this object by [TIMESTEP] seconds
     def Tick(self):
         self.velocity += self.GetNetAcceleration() * C.TIMESTEP
         self.position += self.GetVelocity() * C.TIMESTEP
